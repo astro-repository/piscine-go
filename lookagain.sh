cb73e3ed1ccb8280768b3b7f52d64b85fa94c7d2
cedrick777 <cedrick777@github.com>
2019-06-07 18:34:27 +0000

    lookagain

--- lookagain.sh
+++ lookagain.sh
@@ -1,1 +1,2 @@
-find . \( -type d -o -type f \) -name "*.sh" | sed -e 's/\(\.sh\)$//g' | sed -e 's/^\(\.\/\)//g'
+find . \( -type d -o -type f \) -name "*.sh" | sed -e 
+'s/\(\.sh\)$//g' | sed -e 's/*\///g'
