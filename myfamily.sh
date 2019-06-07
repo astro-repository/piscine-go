b1b472421d69028747dee6c04d2013912c7427fd
cedrick777 <christianastro36@gmail.com>
2019-06-07 18:00:34 +0000

    cl-camp4

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq ".[] | select( .id==70 ) | .connections.relatives" | sed -e 's/"//g'
+curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --argjson id "$HERO_ID " '.[] | select( .id==$id ) | .connections.relatives' | sed -e 's/"//g'
