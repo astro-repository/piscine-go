1f4e7d49a0eda66ee194053f569be04415dca46e
cedrick777 <cedrick777@github.com>
2019-06-08 22:12:03 +0000

    printcomb2

--- printcomb2.go
+++ printcomb2.go
@@ -6,7 +6,7 @@ func PrintComb2() {
 	for a := 0; a <= 9; a++ {
 		for s := 0; s <= 8; s++ {
 			for t := a; t <= 9; t++ {
-				for r := s+1; r <= 9; r++ {
+				for r := 0; r <= 9; r++ {
 					if a==9 && s==8 && t==9 && r==9 {
 						fmt.Printf("%d%d %d%d\n",a,s,t,r)
 					}else{
