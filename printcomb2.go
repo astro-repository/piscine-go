cecebeb3519ff5c53cdc577b9f0adf622769828c
cedrick777 <cedrick777@github.com>
2019-06-08 23:26:51 +0000

    printcomb2

--- printcomb2.go
+++ printcomb2.go
@@ -6,10 +6,10 @@ func PrintComb2() {
 	for i := 0; i <= 97; i++ {
 		for j := 1; j <= 99; j++ {
 			if i<j {
-				if i!=97 && j!=99  {
-					fmt.Printf("%02d %02d, ",i,j)
-				}else{
+				if i==97 && j==99  {
 					fmt.Printf("%02d %02d\n",i,j)
+				}else{
+					fmt.Printf("%02d %02d, ",i,j)
 				}
 			}
 		}
