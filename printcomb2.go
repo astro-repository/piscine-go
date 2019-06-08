197872b4c567e44aa1dc9f4794fcdf2b817b1f4f
cedrick777 <cedrick777@github.com>
2019-06-08 21:18:47 +0000

    printcomb2

---
+++ printcomb2.go
@@ -0,0 +1,22 @@
+package piscine
+
+import "fmt"
+
+func main() {
+	
+}
+func PrintComb2() {
+	for a := 0; a <= 9; a++ {
+		for s := 0; s <= 8; s++ {
+			for t := a; t <= 9; t++ {
+				for r := s+1; r <= 9; r++ {
+					if a==9 && s==8 && t==9 && r==9 {
+						fmt.Printf("%d%d %d%d\n",a,s,t,r)
+					}else{
+						fmt.Printf("%d%d %d%d, ",a,s,t,r)
+					}
+				}
+			}
+		}
+	}
+}
