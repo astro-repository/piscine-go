a83b6ff0a033d73f1078365b74716ef947ed3bf5
cedrick777 <cedrick777@github.com>
2019-06-08 20:44:49 +0000

    printcomb

--- printcomb.go
+++ printcomb.go
@@ -10,7 +10,11 @@ func PrintComb() {
 	for i := 0; i <= 7; i++ {
 		for j := i+1; j <= 8; j++ {
 			for k := j+1; k <= 9; k++ {
-				fmt.Printf("%v%v%v, ",i,j,k)
+				if i==7 && j==8 && k==9 {
+					fmt.Printf("%v%v%v \n",i,j,k)
+				}else{
+					fmt.Printf("%v%v%v, ",i,j,k)
+				}
 			}
 		}
 	}
