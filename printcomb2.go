ce794f7fcb224d7eea081967d5fa35094f16ca06
cedrick777 <cedrick777@github.com>
2019-06-08 22:58:42 +0000

    printcomb2

--- printcomb2.go
+++ printcomb2.go
@@ -3,15 +3,22 @@ package piscine
 import "fmt"
 
 func PrintComb2() {
-	for a := 0; a <= 9; a++ {
-		for s := 0; s <= 8; s++ {
-			for t := a; t <= 9; t++ {
-				for r := 0; r <= 9; r++ {
-					if a==9 && s==8 && t==9 && r==9 {
-						fmt.Printf("%d%d %d%d\n",a,s,t,r)
-					}else{
-						fmt.Printf("%d%d %d%d, ",a,s,t,r)
+	for i := 0; i <= 98; i++ {
+		for j := i+1; j < 99; j++ {
+			if i<10 {
+				if j<10 {
+					fmt.Printf("0%d 0%d",i,j)
+				}else{
+					fmt.Printf("0%d %d",i,j)
+				}
+			}else{
+				if j<10 {
+					fmt.Printf("%d 0%d",i,j)
+				}else{
+					if i==98 && j==99 {
+						fmt.Printf("%d %d\n",i,j)
 					}
+					fmt.Printf("%d %d",i,j)
 				}
 			}
 		}
