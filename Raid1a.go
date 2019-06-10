2d18f59330f5e56ac6500e40103c23c858daebf1
= <=>
2019-06-10 19:18:28 +0000

    Raid1

---
+++ Raid1a.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	student "./student"
+)
+
+func Raid1a(x,y int){
+	for i := 1; i <= y; i++ {
+		if x==1 && y!=1 {
+		for j := 1; j <= y; j++ {
+
+			if j==1 || j==y {
+				print("o\n")
+			}else if j!=1 && j!=y {
+				print("|\n")
+			}
+		}
+		break
+		}else{
+			for j := 1; j <= x; j++ {
+				if (i==1 && j==1) || (i==1&&j==x) || (i==y && j==1) || (i==y && j==x)  {
+					print("o")
+				}else if (i==1 && (j!=1 || j!=x)) || (i==y && (j!=x || j!=x)) {
+					print("-")
+				}else if (i!=1 && j==1) || (i!=y && j==x){
+					print("|")
+				}else{
+					print(" ")
+				}
+			}
+		}
+		print("\n")
+	}
+}
