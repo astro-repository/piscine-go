9772dee4afd7ec00ac14ceb15897dfe433bfce36
= <=>
2019-06-10 19:55:10 +0000

    Raid1n

--- Raid1a.go
+++ Raid1a.go
@@ -1,32 +1,40 @@
-package piscine
+package main
 
 import "fmt"
 
-func Raid1a(x,y int){
-	for i := 1; i <= y; i++ {
-		if x==1 && y!=1 {
-		for j := 1; j <= y; j++ {
+func main(){
+	Raid1a(-1,6)
+}
 
-			if j==1 || j==y {
-				fmt.Print("o\n")
-			}else if j!=1 && j!=y {
-				fmt.Print("|\n")
-			}
-		}
-		break
-		}else{
-			for j := 1; j <= x; j++ {
-				if (i==1 && j==1) || (i==1&&j==x) || (i==y && j==1) || (i==y && j==x)  {
-					fmt.Print("o")
-				}else if (i==1 && (j!=1 || j!=x)) || (i==y && (j!=x || j!=x)) {
-					fmt.Print("-")
-				}else if (i!=1 && j==1) || (i!=y && j==x){
-					fmt.Print("|")
-				}else{
-					fmt.Print(" ")
+func Raid1a(x,y int){
+
+	if x>0 && y>0 {
+
+		for i := 1; i <= y; i++ {
+			if x==1 && y!=1 {
+			for j := 1; j <= y; j++ {
+
+				if j==1 || j==y {
+					fmt.Print("o\n")
+				}else if j!=1 && j!=y {
+					fmt.Print("|\n")
 				}
 			}
+			break
+			}else{
+				for j := 1; j <= x; j++ {
+					if (i==1 && j==1) || (i==1&&j==x) || (i==y && j==1) || (i==y && j==x)  {
+						fmt.Print("o")
+					}else if (i==1 && (j!=1 || j!=x)) || (i==y && (j!=x || j!=x)) {
+						fmt.Print("-")
+					}else if (i!=1 && j==1) || (i!=y && j==x){
+						fmt.Print("|")
+					}else{
+						fmt.Print(" ")
+					}
+				}
+			}
+			fmt.Print("\n")
 		}
-		fmt.Print("\n")
 	}
 }
