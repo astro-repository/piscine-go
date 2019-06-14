80a9a7b8ad28c56d4dd07f1d6ccb8487a8427e14
= <=>
2019-06-14 00:08:01 +0000

    quest 04

--- iterativepower.go
+++ iterativepower.go
@@ -2,7 +2,7 @@ package piscine
 
 func IterativePower(nb, power int) int {
 	exposant := 1
-	if power<0 || (nb==0 && power == 0) {
+	if power<0 {
 		return 0
 	}else{
 		for i := 0; i < power; i++ {
