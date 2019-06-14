1ebe270ff0dc7f5f9195d8214ac64211af8ce3c4
= <=>
2019-06-14 00:21:44 +0000

    quest 04

--- fibonacci.go
+++ fibonacci.go
@@ -1,10 +1,11 @@
 package piscine
-
 func Fibonacci(arg1 int) int{
 	if arg1 == 1 || arg1 == 2 {
 		return 1
 	}else if arg1 < 0{
 		return -1
+	}else if arg1 == 0{
+		return 0
 	}else{
 		return Fibonacci(arg1-2) + Fibonacci(arg1-1)
 	}
