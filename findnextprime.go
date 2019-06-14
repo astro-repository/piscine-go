84d9a453980550301b1522aa08048c2adc1e520f
= <=>
2019-06-14 00:59:45 +0000

    quest 04

--- findnextprime.go
+++ findnextprime.go
@@ -1,7 +1,12 @@
 package piscine
 
+import "math"
+
 func FindNextPrime(nb int) int{
 	compteur := 0
+	if math.MaxInt32 < nb || math.MaxInt32 - 3 < nb{
+		nb = 0
+	}
 	for i := 1; i <= nb; i++ {
 		if nb % i == 0{
 			compteur++
