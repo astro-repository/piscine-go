c368baedff74f78abd31651716db07439e5170b1
= <=>
2019-06-15 04:09:28 +0000

    Quest 05

--- isalpha.go
+++ isalpha.go
@@ -3,7 +3,7 @@ package piscine
 func IsAlpha(s string) bool {
 	sRune := []rune(s)
 	for _, v := range sRune {
-		if !(v >= 97 && v <= 122) && !(90 >= v && v >= 65) {
+		if !(v >= 97 && v <= 122) && !(90 >= v && v >= 65) && !(48 <= v && v <= 57) {
 			return false
 		}
 	}
