0c859823e33f94f89076ab37f3c2fac37abab7fd
= <=>
2019-06-15 04:23:50 +0000

    Quest 05

--- isprintable.go
+++ isprintable.go
@@ -3,8 +3,8 @@ package piscine
 func IsPrintable(s string) bool {
 	sRune := []rune(s)
 	for _, v := range sRune {
-		if !(v >= 90 && v <= 65) {
-			return true
+		if v==92 && (v >= 97 && v <= 122) {
+			return false
 		}
 	}
 	return true
