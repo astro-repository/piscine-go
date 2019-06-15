91298edaedbfcdfbfa35907725dd1b467a66434c
= <=>
2019-06-15 03:12:45 +0000

    Quest 05

--- tolower.go
+++ tolower.go
@@ -4,7 +4,7 @@ func ToLower(s string) string {
 	sRune := []rune(s)
 	distance := 'a' - 'A'
 	for i, v := range sRune {
-		if v >= 41 && v <= 90 {
+		if v >= 65 && v <= 90 {
 			sRune[i] = v + distance
 		}
 	}
