49f7f01c14cfa6efe25289ebda21c3545b4c97ec
= <=>
2019-06-16 05:36:13 +0000

    Quest 07

--- appendrange.go
+++ appendrange.go
@@ -1,12 +1,10 @@
-package piscine
-
 func AppendRange(min, max int) []int {
-	var tableau []int
-	if min>=max{
+	tableau := []int{};
+	for i := min; i < max ; i++ {
+		tableau = append(tableau,i);
+	}
+	if min>=max {
 		return nil
 	}
-	for i := min; i < max ; i++ {
-		tableau = append(tableau,i)
-	}
-	return tableau
+	return tableau;
 }
