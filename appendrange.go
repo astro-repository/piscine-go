9a45edf9b02b0537c9164d6816b1022968bee304
= <=>
2019-06-16 04:35:48 +0000

    Quest 07

--- appendrange.go
+++ appendrange.go
@@ -16,10 +16,21 @@
 +	GITHUB : github.com/cedrick777				+						+
 + +++++++++++++++++++++++++++++++++++++++++++++ +++++++++++++++++++++++*/
 
-package piscine
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(AppendRange(4900,5000))
+	fmt.Println(AppendRange(10, 5))
+	fmt.Println(AppendRange(0, 0))
+}
 
 func AppendRange(min, max int) []int {
-	tableau := []int{}
+	if min>max {
+		return []int{}
+	}
+	tableau := make([]int, max-min)
 	for i := min; i < max ; i++ {
 		tableau = append(tableau,i,)
 	}
