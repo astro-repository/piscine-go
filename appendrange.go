97b6d9a449deaa4aa3c37e7f9c57a7bad1766046
= <=>
2019-06-16 04:05:39 +0000

    Quest 07

--- appendrange.go
+++ appendrange.go
@@ -1,7 +1,33 @@
-package piscine
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
++																		+
++		**		**********	* ****** *	**********	   *****			+
++	**		**	**		**	*** ** ***	**	**	**	***		***			+
++	**		**	***				**		**		**	***		***			+
++	** ****	**		** ***		**		**	**		***		***			+
++	**		**			**		**		**		**	***		***			+
++	**		**	**********		**		**		**	   *****			+
++																		+
++ +++++++++++++++++++++++++++++++++++++++++++++	+						+
++					ZONE-01						+						+
++ +++++++++++++++++++++++++++++++++++++++++++++	+						+
++	NOM : TOURE 								+						+
++	PRENOMS : Ahmed Christian Cédrick			+						+
++	QUEST 06 : sortparams						+						+
++	GITHUB : github.com/cedrick777				+						+
++ +++++++++++++++++++++++++++++++++++++++++++++ +++++++++++++++++++++++*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(AppendRange(4900,5000))
+	fmt.Println(AppendRange(10, 5))
+	fmt.Println(AppendRange(0, 0))
+}
 
 func AppendRange(min, max int) []int {
-	tableau := []int{}
+	var tableau []int
 	for i := min; i < max ; i++ {
 		tableau = append(tableau,i)
 	}
