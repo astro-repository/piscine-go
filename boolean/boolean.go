03d6e793302aa7ef98fff9961843aa2165fc9df7
= <=>
2019-06-20 09:20:22 +0000

    Quest 08

--- boolean/boolean.go
+++ boolean/boolean.go
@@ -12,42 +12,26 @@
 '---------------------------------------'----------*/
 package main
 
-import "github.com/01-edu/z01"
-import "os"
-
-func printStr(str string) {
-	arrayStr := []rune(str)
-
-	for i := 0; i < len(arrayStr); i++ {
-		z01.PrintRune(arrayStr[i])
-	}
-	z01.PrintRune('\n')
-}
+import (
+	"fmt"
+	"os"
+)
 
 func isEven(nbr int) bool {
-	if even(nbr%2) == 1 {
+	if nbr%2 == 0 {
 		return true
 	} else {
 		return false
 	}
 }
 
-func even(nbr int) int {
-	if even(nbr % 2) == 0 {
-		return 1
-	} else {
-		return 0
-	}
-}
-
 func main() {
 	EvenMsg := "I have an even number of arguments"
 	OddMsg := "I have an odd number of arguments"
-	lengthOfArg := len(os.Args[1:len(os.Args)])
-
+	lengthOfArg := len(os.Args[1:])
 	if isEven(lengthOfArg) {
-		printStr(EvenMsg)
+		fmt.Println(EvenMsg)
 	} else {
-		printStr(OddMsg)
+		fmt.Println(OddMsg)
 	}
 }
