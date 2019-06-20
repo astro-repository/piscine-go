5d6964472adef33da9f646839a6e0a72bd12671a
= <=>
2019-06-20 09:43:37 +0000

    Quest 08

---
+++ point/point.go
@@ -0,0 +1,35 @@
+/*--------------------------------------------------.
+|     **      ******  ********  *******     ****    |
+|  **    **  *.          **     **   **   **    **  |
+|  **    **     ***.     **     ** ***    **    **  |
+|  ********  *      *    **     **   **   **    **  |
+|  **    **   ******     **     **   **     ****    |
+|---------------------------------------.           |
+|          ZONE-01 (cedrick777)         |           |
+|---------------------------------------|           |
+|  NOM : TOURE Ahmed Christian Cédrick  |           |
+|  QUEST 08 : point                     |           |
+'---------------------------------------'----------*/
+package main
+
+import (
+	"fmt"
+)
+
+type point struct {
+	x int
+	y int
+}
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
+}
+
+func main() {
+	points := &point{}
+
+	setPoint(points)
+
+	fmt.Printf("x = %d, y = %d\n",points.x, points.y)
+}
