9a87d4e9832de0d63793b48d054dc6f23d308d54
Touré Ahmed Chrisitan Cédrick <47228043+cedrick777@users.noreply.github.com>
2019-06-21 22:52:03 +0000

    Update doop.go

--- doop/doop.go
+++ doop/doop.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	if len(os.Args)>=3 {
+	if len(os.Args)==4 {
 		os.Args = os.Args[1:]
 		a, errA := strconv.Atoi(os.Args[0])
 		b, errB := strconv.Atoi(os.Args[2])
@@ -18,7 +18,7 @@ func main() {
 			switch os.Args[1] {
 			case "+": fmt.Println(a + b)
 			case "*": fmt.Println(a * b)
-			case "p": fmt.Println(a - b)
+			case "-": fmt.Println(a - b)
 			case "/": 
 				if b != 0 {
 					fmt.Println(a/b)
@@ -33,7 +33,5 @@ func main() {
 				}
 			}
 		}
-	}else{
-		fmt.Println(0)
 	}
 }
