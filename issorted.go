cecd0744c0278a9edfd5c76c7fed133db38e3b16
= <=>
2019-06-21 21:33:44 +0000

    Quest 09

--- issorted.go
+++ issorted.go
@@ -13,8 +13,25 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, tab []int) bool {
+	if !croissant(f, tab) && !decroissant(f, tab) {
+		return false
+	}else{
+		return true
+	}
+
+}
+func croissant(f func(a, b int) int, tab []int) bool {
 	for i := 0; i < len(tab)-1; i++ {
 		if f(tab[i],tab[i+1]) > 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func decroissant(f func(a, b int) int, tab []int) bool {
+	for i := len(tab)-1; i >= 0; i-- {
+		if f(tab[i],tab[i-1]) > 0 {
 			return false
 		}
 	}
