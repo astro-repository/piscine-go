2075a0f4b8eca19d013e1d78c631e9071c20a220
Touré Ahmed Chrisitan Cédrick <47228043+cedrick777@users.noreply.github.com>
2019-06-21 23:15:36 +0000

    Update doop.go

--- doop/doop.go
+++ doop/doop.go
@@ -11,27 +11,31 @@ func main() {
 		os.Args = os.Args[1:]
 		a, errA := strconv.Atoi(os.Args[0])
 		b, errB := strconv.Atoi(os.Args[2])
-		if errA != nil || errB != nil{
-			fmt.Println(0)
-		}else{
-			
-			switch os.Args[1] {
-			case "+": fmt.Println(a + b)
-			case "*": fmt.Println(a * b)
-			case "-": fmt.Println(a - b)
-			case "/": 
-				if b != 0 {
-					fmt.Println(a/b)
-				}else{
-					fmt.Println("No division by 0")
-				}
-			case "%": 
-				if b != 0 {
-					fmt.Println(a%b)
-				}else{
-					fmt.Println("No modulo by 0")
-				}
+		if errA != nil {
+			a := 0
+		}
+		if errB != nil {
+			b := 0
+		}
+
+		switch os.Args[1] {
+		case "+": fmt.Println(a + b)
+		case "*": fmt.Println(a * b)
+		case "-": fmt.Println(a - b)
+		case "/": 
+			if b != 0 {
+				fmt.Println(a/b)
+			}else{
+				fmt.Println("No division by 0")
 			}
+		case "%": 
+			if b != 0 {
+				fmt.Println(a%b)
+			}else{
+				fmt.Println("No modulo by 0")
+			}
+		default :
+		fmt.Println(0)
 		}
 	}
 }
