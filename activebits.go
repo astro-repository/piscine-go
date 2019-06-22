5911524ccf54dc8c83fecdd9944ad0d8511f06ab
Touré Ahmed Chrisitan Cédrick <47228043+cedrick777@users.noreply.github.com>
2019-06-22 13:57:26 +0000

    Update activebits.go

--- activebits.go
+++ activebits.go
@@ -1,17 +1,15 @@
 package piscine
 
-import "strconv"
-
 func ActiveBits(n int) uint {
 
 	a:= int64(n)
-	nbin := strconv.FormatInt(a, 2)
+	nbin := (a, 2)
 	compteur := 0
 	for _, v := range nbin {
 		if v == '1' {
 			compteur++
 		}
 	}
-	return uint(compteur)
+	return compteur
 
 }
