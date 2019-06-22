36d7388997a4cae9ec99ab823c7de8c875186294
Touré Ahmed Chrisitan Cédrick <47228043+cedrick777@users.noreply.github.com>
2019-06-22 14:53:37 +0000

    Update unmatch.go

--- unmatch.go
+++ unmatch.go
@@ -1,40 +1,22 @@
 package piscine
 
 func Unmatch(arr []int) int {
-	arrT := Maxm(arr)
-
-	for i := 0; i < len(arrT)-1; i++ {
-		for j, v := range arrT {
-			if arrT[i] == v {
-				arrT[i] = 0
-				arrT[j] = 0
-				break
-			}
-
-			if i==len(arrT)-1 {
-				return arrT[i]
+	table := make([]int, len(arr))
+	
+	for i := 0; i < len(arr) - 1; i++ {
+		compteur := 0
+		for j := 1; j < len(arr) - 1; j++ {
+			if arr[i] == arr[j] {
+				compteur++
 			}
 		}
+		table[i] = compteur
 	}
 
-	for _, v := range arrT {
-		if v != 0 {
-			return v
+	for i, v := range table {
+		if v == 0 {
+			return arr[i]
 		}
 	}
-
 	return 0
-}
-
-func Maxm(table []int) []int {
-	for i := 0; i < len(table)-1; i++ {
-		for j := 0; j < len(table)-1; j++ {
-			if table[j] > table[j+1]{
-				intermediaire := table[j]
-				table[j] = table[j+1]
-				table[j+1] = intermediaire
-			}
-		}
-	}
-	return table
 }
