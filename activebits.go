3d41a6755bd1d4f1f8d1a6e0b33946746511e3a3
Touré Ahmed Chrisitan Cédrick <47228043+cedrick777@users.noreply.github.com>
2019-06-22 13:55:49 +0000

    Update activebits.go

--- activebits.go
+++ activebits.go
@@ -1,8 +1,15 @@
 package piscine
 
 func ActiveBits(n int) uint {
-	if n<0 {
-		return uint(-n)
+
+	a:= int64(n)
+	nbin := strconv.FormatInt(a, 2)
+	compteur := 0
+	for _, v := range nbin {
+		if v == '1' {
+			compteur++
+		}
 	}
-	return uint(3)
+	return uint(compteur)
+
 }
