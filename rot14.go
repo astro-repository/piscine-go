9a4daf907bc452aea5ba3d26d2cbd58f92e804ed
= <=>
2019-06-22 09:40:51 +0000

    Examen

---
+++ rot14.go
@@ -0,0 +1,68 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	fmt.Println(Rot14("Hello How are You"))
+	
+}
+
+func Rot14(str string) string {
+	Maj := alphabetMaj()
+	Min := alphabetMin()
+	strrune := []rune(str)
+	for _, v := range strrune {
+		if 'A' <= v && v <= 'Z' {
+			index := returnIndex(v) + 1
+			sliceMaj := Maj[index:]
+			sliceMajRestant := Maj[:index]
+			hachage := append(sliceMaj,sliceMajRestant...)
+			fmt.Print(string(hachage[13]))
+		}
+		
+		if 'a' <= v && v <= 'z' {
+			index := returnIndex(v) + 1
+			sliceMin := Min[index:]
+			sliceMinRestant := Min[:index]
+			hach := append(sliceMin,sliceMinRestant...)
+			fmt.Print(string(hach[13]))
+		}
+
+		if v == ' ' {
+			fmt.Print(" ")
+		}
+	}
+	return ""
+}
+
+func alphabetMaj() []rune  {
+	Maj := []rune{}
+	for i := 'A'; i <= 'Z'; i++ {
+		Maj = append(Maj, i)
+	}
+	return Maj
+}
+
+func alphabetMin() []rune  {
+	Min := []rune{}
+	for i := 'a'; i <= 'z'; i++ {
+		Min = append(Min, i)
+	}
+	return Min
+}
+
+func returnIndex(car rune) int {
+	for i, v := range alphabetMaj() {
+		if car == v {
+			return i
+		}
+	}
+
+	for i, v := range alphabetMin() {
+		if car == v {
+			return i
+		}
+	}
+	return -1
+}
