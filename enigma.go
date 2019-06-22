b4d459053ae3a258f16604bd3b11b74b7c41b989
Touré Ahmed Chrisitan Cédrick <47228043+cedrick777@users.noreply.github.com>
2019-06-22 12:10:31 +0000

    Update enigma.go

--- enigma.go
+++ enigma.go
@@ -3,9 +3,9 @@ package piscine
 func Enigma(a ***int, b *int, c *******int, d ****int) {
 	permC:=*******c
 	permD:=****d
-	permC=*b
+	permB=*b
 	*******c=***a
 	****d=permC
 	*b=permD
-	***a=permC
+	***a=permB
 }
