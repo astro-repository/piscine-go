53b61dc10875656e5d56d447aeef413939333bc4
Touré Ahmed Chrisitan Cédrick <47228043+cedrick777@users.noreply.github.com>
2019-06-22 14:58:06 +0000

    Update unmatch.go

--- unmatch.go
+++ unmatch.go
@@ -3,18 +3,17 @@ package piscine
 func Unmatch(arr []int) int {
 	table := make([]int, len(arr))
 	
-	for i := 0; i < len(arr) - 1; i++ {
+	for i := 0; i < len(arr); i++ {
 		compteur := 0
-		for j := 1; j < len(arr) - 1; j++ {
+		for j := 0; j < len(arr); j++ {
 			if arr[i] == arr[j] {
 				compteur++
 			}
 		}
 		table[i] = compteur
 	}
-
 	for i, v := range table {
-		if v == 0 {
+		if v == 1 {
 			return arr[i]
 		}
 	}
