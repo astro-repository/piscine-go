93d45705dc09b5c4fc57139d33716ea2add8cc6c
Touré Ahmed Chrisitan Cédrick <47228043+cedrick777@users.noreply.github.com>
2019-06-22 13:07:44 +0000

    Add files via upload

---
+++ fixthemain/main.go
@@ -0,0 +1,66 @@
+package main
+
+import "fmt"
+
+func PrintStr(str string) {
+	arrayRune := []rune(str)
+	for _, s := range arrayRune {
+		fmt.Printf(string(s))
+	}
+	fmt.Println()
+}
+
+
+
+func CloseDoor(ptrDoor *Door) {
+	
+	PrintStr("Door Closing...")
+	ptrDoor.state = false
+	// state = CLOSE
+	// return true
+}
+
+func IsDoorOpen(door *Door) bool {
+	if door.state {
+		return true
+	}else{
+		return false
+	}
+	// const OPEN := "open" 
+	// PrintStr("is the Door opened ?")
+	// return Door.state = OPEN
+}
+
+func IsDoorClose(ptrDoor *Door) bool {
+	PrintStr("is the Door closed ?")
+	return ptrDoor.state
+}
+
+type Door struct {
+	state bool
+}
+
+func OpenDoor(door *Door){
+	door.state = true
+}
+
+// func CloseDoor(door *Door) {
+// 	door.state = false
+// }
+
+func main() {
+	const OPEN = true
+
+	door := &Door{}
+
+	OpenDoor(door)
+	if IsDoorClose(door) {
+		OpenDoor(door)
+	}
+	if IsDoorOpen(door) {
+		CloseDoor(door)
+	}
+	if door.state == OPEN {
+		CloseDoor(door)
+	}
+}
