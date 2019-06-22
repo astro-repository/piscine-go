fc35dec4cbf432a49f9577c36a8242be654a1e1d
= <=>
2019-06-22 11:33:23 +0000

    Exam

---
+++ comcheck/comcheck.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+import "os"
+
+func main() {
+	if len(os.Args)>1 {
+		os.Args = os.Args[1:]
+		juge := false
+		for _, v := range os.Args {
+			if v == "01" || v == "galaxy" || v == "galaxy 01" {
+				juge = true
+			}
+		}
+		if juge {
+			fmt.Println("Alert!!!")
+		}else{
+			fmt.Println()
+		}
+	}else{
+		fmt.Println()
+	}
+}
+
