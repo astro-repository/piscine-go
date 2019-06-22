cd988aa4ce2bae9bbd6fc0375defb58254a0d572
= <=>
2019-06-22 10:17:59 +0000

    Examen

--- abort.go
+++ abort.go
@@ -1,5 +1,16 @@
 package piscine
 
 func Abort(a, b, c, d, e int) int {
-	return (a+b+c+d+e)/5
+	table := []int{a, b, c, d, e}
+
+	for i := 0; i < len(table)-1; i++ {
+		for j := 0; j < len(table)-1; j++ {
+			if table[j] > table[j+1]{
+				intermediaire := table[j]
+				table[j] = table[j+1]
+				table[j+1] = intermediaire
+			}
+		}
+	}
+	return table[2]
 }
