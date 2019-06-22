d00d97beadacca4d7ba2b13244dbdab799084254
= <=>
2019-06-22 11:08:19 +0000

    Exam

---
+++ collatzcountdown.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(CollatzCountdown(12))
+}
+
+func CollatzCountdown(start int) int {
+	compteur := 1
+	return Compteur(compteur, start)
+}
+
+
+func Compteur(compteur int, start int) int {
+
+	if start == 1{
+		return compteur
+	}
+	
+	if start % 2 == 0 {
+		start = start / 2
+		compteur++
+		return Compteur(compteur, start)
+	}else{
+		start = (start * 3)+1
+		compteur++
+		return Compteur(compteur, start)
+	}
+
+	return compteur
+}
