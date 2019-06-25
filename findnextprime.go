d8842a8b28214ca69dfb7d1cb8d41685c6fa6bec
Touré Ahmed Chrisitan Cédrick <47228043+cedrick777@users.noreply.github.com>
2019-06-25 11:17:51 +0000

    Update findnextprime.go

--- findnextprime.go
+++ findnextprime.go
@@ -1,55 +1,17 @@
 package piscine
 
-func isPrime(nb int) bool{
-	compteur := 0
-		for i := 1; i <= nb; i++ {
-			if nb % i == 0{
-				compteur++
-			}
-			if compteur == 2 && i==nb {
-				return true
-			}
-			if compteur == 2 && i!=nb {
-				return false
-			}
-	}
-	return false
-}
+import "math"
 
-func isNextPrime(nb,appel int) bool{
-	appel++
-	compteur := 0
-		for i := 1; i <= nb; i++ {
-			if nb % i == 0{
-				compteur++
-			}
-			if compteur == 2 && i==nb {
-				return true
-			}
-			if compteur == 2 && i!=nb {
-				return false
-			}
-	}
-	return false
-}
-
-func FindNextPrime(nb int) int {
-	if nb < 19000000{
-		if isPrime(nb){
-			return nb
-		}else{
-			i:=0
-			for !isPrime(nb) {
-				i++
-				nb++
-				if i > 20 {
-					return 2
-				}
-				isNextPrime(nb,i)
-			}
-			return nb
-		}
-	}else{
+func FindNextPrime(nb int) int{
+	if nb<1{
 		return 2
+	}else{
+		for i:=2 ; i<= int( math.Sqrt(float64(nb)) ); i++{
+			if nb%i==0{
+				nb++
+				FindNextPrime(nb)
+			}
+		}
 	}
+	return nb
 }
