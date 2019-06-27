e7c9edd3f9c06662d73e07638fb3f66e21454df2
Touré Ahmed Chrisitan Cédrick <47228043+cedrick777@users.noreply.github.com>
2019-06-27 14:04:17 +0000

    Add files via upload

---
+++ listlast.go
@@ -0,0 +1,25 @@
+/*--------------------------------------------------.
+|     **      ******  ********  *******     ****    |
+|  **    **  *.          **     **   **   **    **  |
+|  **    **     ***.     **     ** ***    **    **  |
+|  ********  *      *    **     **   **   **    **  |
+|  **    **   ******     **     **   **     ****    |
+|---------------------------------------.           |
+|          ZONE-01 (cedrick777)         |           |
+|---------------------------------------|           |
+|  NOM : TOURE Ahmed Christian Cédrick  |           |
+|  QUEST 11 : listlast                  |           |
+'---------------------------------------'----------*/
+package piscine
+
+func ListLast(link *List) interface{} {
+
+	for link.Head != nil {
+		if link.Head.Next == nil {
+			return link.Head.Data
+		}
+		link.Head = link.Head.Next
+	}
+
+	return link.Head
+}
