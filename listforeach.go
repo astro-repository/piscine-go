47486f99cce2d7a1c50c08cae9fea77a7b217e71
Touré Ahmed Chrisitan Cédrick <47228043+cedrick777@users.noreply.github.com>
2019-06-28 12:12:40 +0000

    Update listforeach.go

--- listforeach.go
+++ listforeach.go
@@ -11,3 +11,21 @@ func ListForEach(l *List, f func(*NodeL)) {
 	l.Head = l.Tail
 
 }
+
+func Add2_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) + 2
+	case string:
+		node.Data = node.Data.(string) + "2"
+	}
+}
+
+func Subtract3_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) - 3
+	case string:
+		node.Data = node.Data.(string) + "-3"
+	}
+}
